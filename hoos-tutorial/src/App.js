4dfb57160ab8c18f7715179b7c9bf42d64f9b4ec
BeomSu-Kang <kbs21c1987@gmail.com>
2022-05-06 17:34:17 +0900

    리듀서 카운터구현

--- hoos-tutorial/src/App.js
+++ hoos-tutorial/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import Info from "./Info.js";
+// import Info from "./Info.js";
+import Counter2 from "./Counter2";
 
-function App() {
-  return <Info />;
-}
+const App = () => {
+  return <Counter2 />;
+};
 
 export default App;
