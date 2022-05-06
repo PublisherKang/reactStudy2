00d88f467c1db8bcac502af6b40706056bc0d688
BeomSu-Kang <kbs21c1987@gmail.com>
2022-05-06 17:43:16 +0900

    리듀서 인풋 상태관리

--- hoos-tutorial/src/App.js
+++ hoos-tutorial/src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 // import Info from "./Info.js";
-import Counter2 from "./Counter2";
+import Info2 from "./Info2.js";
+// import Counter2 from "./Counter2";
 
 const App = () => {
-  return <Counter2 />;
+  return <Info2 />;
 };
 
 export default App;
