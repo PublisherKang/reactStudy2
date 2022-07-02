d8ce8018bf3aef8d2bb2b6f438b3f2a1ee8581db
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-02 18:23:35 +0900

    하이퍼링크 Link 사용하기

--- router-tutorial/src/pages/About.js
+++ router-tutorial/src/pages/About.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <Link to="/">이전</Link>
     </div>
   );
 };
