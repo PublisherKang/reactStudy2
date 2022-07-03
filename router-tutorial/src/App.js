fc665eadb2de14211ec7a2e2e24b3a2c6c009b19
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-03 19:45:09 +0900

    로그인페이지 만들어 보기, Navigate를 이용해 mypage접속시 로그인페이지로 강제이동 구현

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -6,6 +6,8 @@ import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
   return (
@@ -19,9 +21,10 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
       <Route path="*" element={<NotFound />} />
       {/* "*"는 wildcard 문자이다. 엘리먼트 상단에 위치하는 라우트의 규칙을 확인하고 일치하는 라우트가 없으면 이 라우트가 화면에 나타남 */}
-      
     </Routes>
   );
 };
