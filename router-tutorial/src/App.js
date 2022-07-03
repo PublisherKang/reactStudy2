e7002547d390422881732001e3ccafb07fc121dd
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-03 19:04:54 +0900

    NotFound 페이지 만들기1

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 const App = () => {
@@ -18,6 +19,9 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
+      {/* "*"는 wildcard 문자이다. 엘리먼트 상단에 위치하는 라우트의 규칙을 확인하고 일치하는 라우트가 없으면 이 라우트가 화면에 나타남 */}
+      
     </Routes>
   );
 };
