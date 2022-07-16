fba6d32a56056361326bc6918a3445f1f5ebcb28
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-16 15:49:32 +0900

    카테고리 스타일 지정하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,11 +1,17 @@
+import { useState, useCallback } from 'react';
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
 
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  console.log(category);
+
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   );
 };
