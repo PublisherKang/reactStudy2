d99d63e60ecd0e15829757f6dd78d0b97a7c1ce3
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-17 18:52:47 +0900

    promise 사용해보기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios'; //axios 불러오기
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -26,34 +27,50 @@ const NewsListBlock = styled.div`
 // };
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [articles, setArticles] = useState(null);
+  // const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=bbe9b029ef2b4d12b2d5d1316353d975`);
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `category=${category}`;
+    return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=bbe9b029ef2b4d12b2d5d1316353d975`);
   }, [category]);
+
+  // useEffect(() => {
+  //   // async를 사용하는 함수 따로 선언
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const query = category === 'all' ? '' : `&category=${category}`;
+  //       const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=bbe9b029ef2b4d12b2d5d1316353d975`);
+  //       setArticles(response.data.articles);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, [category]);
 
   //대기중일때
   if (loading) {
     return <NewsListBlock>대기중...</NewsListBlock>;
   }
 
-  // 아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // 아직 articles 값이 설정되지 않았을 때
+  // if (!articles) {
+  //   return null;
+  // }
+
+  // response값이 유효 할때
+  const { articles } = response.data;
 
   //articles 값이 유효할 때
   return (
