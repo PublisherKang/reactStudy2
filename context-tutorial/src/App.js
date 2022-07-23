e06e68b731321493aa6dbdba8575d4b6da592f57
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-23 16:41:23 +0900

    Provider 사용해보기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,10 +1,14 @@
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: "red" }}>
+      {/* value를 지정하지 않으면 오류가 남. */}
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
