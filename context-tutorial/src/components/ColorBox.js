cdf1ec1a61066c6c82603bdfad46f5ec5bd400db
BeomSu-Kang <kbs21c1987@gmail.com>
2022-07-23 17:35:27 +0900

    Hook으로 색상 선택 컴포넌트 만들기

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,48 @@
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext, useState } from "react";
+import ColorContext from "../contexts/color";
+//import { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
+
     //    <ColorContext.Consumer>
-    <ColorConsumer>
-      {(value) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: value.state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: value.state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    // <ColorConsumer>
+    //   {(value) => (
+    //     <>
+    //       <div
+    //         style={{
+    //           width: "64px",
+    //           height: "64px",
+    //           background: value.state.color,
+    //         }}
+    //       />
+    //       <div
+    //         style={{
+    //           width: "32px",
+    //           height: "32px",
+    //           background: value.state.subcolor,
+    //         }}
+    //       />
+    //     </>
+    //   )}
+    // </ColorConsumer>
     //  </ColorContext.Consumer>
   );
 };
